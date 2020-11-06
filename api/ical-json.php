81dd5df7f2eb4a833d3a8da41fa69a6f7a8a2e37
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-11-06 10:11:12 +0100

    ican-json in two separate files

--- api/ical-json.php
+++ api/ical-json.php
@@ -1,57 +1,21 @@
 <?php
+
+include_once("./includes/ical-json.php");
+
 header('Content-Type: application/json');
-
-class EDT
-{
-    public $weeks = array();
-    public $success = true;
-}
-
-class Week
-{
-    public $days = array();
-}
-
-class Day
-{
-    public $events = array();
-}
-
-$edt = new EDT;
 
 if (isset($_GET['url']) and !empty($_GET['url'])) {
     $url = $_GET['url'];
     $weeks = 2;
-    $events = array();
     $offset = 0;
     if (isset($_GET['weeks']) and !empty($_GET['weeks']))
         $weeks = intval($_GET['weeks']);
     if (isset($_GET['offset']) and !empty($_GET['offset']))
         $offset = intval($_GET['offset']);
-    $raw = file_get_contents("https://ical-to-json.herokuapp.com/convert.json?url=$url");
-    if ($raw != false) {
-        $rawevents = json_decode($raw)->vcalendar[0]->vevent;
-        for ($i = 0; $i < $weeks; $i++) {
-            $edt->weeks[] = new Week;
-        }
-        $lastDate = "";
-        foreach ($rawevents as $event) {
-            $rawtime = substr($event->dtstart[0], 0, strlen('YYYYmmdd'));
-            for ($i = $offset; $i < $offset + $weeks; $i++) {
-                $j = $i - 1;
-                $currMon = date('Ymd', strtotime("monday $j week"));
-                if ($currMon <= $rawtime &&  $rawtime <= date('Ymd', strtotime("saturday $i week"))) {
-                    if (strcmp($lastDate, $rawtime) != 0) {
-                        $lastDate = $rawtime;
-                        $edt->weeks[$i]->days[] = new Day;
-                    }
-                    end($edt->weeks[$i]->days)->events[] = $event;
-                }
-            }
-        }
-    } else
-        $edt->success = 'failed on getting distant JSON';
+    echo ical_to_json($url, $weeks, $offset);
 } else
-    $edt->success = 'no url provided';
-
-echo json_encode($edt);
+    echo json_encode(new class ()
+    {
+        public $weeks = [];
+        public $success = 'no url provided';
+    });
---
+++ api/includes/ical-json.php
@@ -0,0 +1,49 @@
+<?php
+
+class EDT
+{
+    public $weeks = array();
+    public $success = true;
+}
+
+class Week
+{
+    public $days = array();
+}
+
+class Day
+{
+    public $events = array();
+}
+
+
+function ical_to_json($url, $weeks, $offset)
+{
+    $edt = new EDT;
+    $events = array();
+    $raw = file_get_contents("https://ical-to-json.herokuapp.com/convert.json?url=$url");
+    if ($raw != false) {
+        $rawevents = json_decode($raw)->vcalendar[0]->vevent;
+        for ($i = 0; $i < $weeks; $i++) {
+            $edt->weeks[] = new Week;
+        }
+        $lastDate = "";
+        foreach ($rawevents as $event) {
+            $rawtime = substr($event->dtstart[0], 0, strlen('YYYYmmdd'));
+            for ($i = $offset; $i < $offset + $weeks; $i++) {
+                $j = $i - 1;
+                $currMon = date('Ymd', strtotime("monday $j week"));
+                if ($currMon <= $rawtime &&  $rawtime <= date('Ymd', strtotime("saturday $i week"))) {
+                    if (strcmp($lastDate, $rawtime) != 0) {
+                        $lastDate = $rawtime;
+                        $edt->weeks[$i]->days[] = new Day;
+                    }
+                    end($edt->weeks[$i]->days)->events[] = $event;
+                }
+            }
+        }
+    } else
+        $edt->success = 'failed on getting distant JSON';
+
+    return json_encode($edt);
+}
