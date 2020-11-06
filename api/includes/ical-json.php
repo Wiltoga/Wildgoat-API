be4b560f0e74a457827491266fe68f534894feba
Nathan HANEN <30344403+WildGoat07@users.noreply.github.com>
2020-11-06 11:58:06 +0100

    json includes all day of the week
    previously, it didn't have empty days and the sunday

--- api/includes/ical-json.php
+++ api/includes/ical-json.php
@@ -8,7 +8,19 @@ class EDT
 
 class Week
 {
-    public $days = array();
+    public function __construct()
+    {
+        $this->days = array(
+            new Day,
+            new Day,
+            new Day,
+            new Day,
+            new Day,
+            new Day,
+            new Day
+        );
+    }
+    public $days;
 }
 
 class Day
@@ -20,25 +32,20 @@ class Day
 function ical_to_json($url, $weeks, $offset)
 {
     $edt = new EDT;
-    $events = array();
     $raw = file_get_contents("https://ical-to-json.herokuapp.com/convert.json?url=" . urlencode($url));
     if ($raw != false) {
         $rawevents = json_decode($raw)->vcalendar[0]->vevent;
         for ($i = 0; $i < $weeks; $i++) {
             $edt->weeks[] = new Week;
         }
-        $lastDate = "";
         foreach ($rawevents as $event) {
             $rawtime = substr($event->dtstart[0], 0, strlen('YYYYmmdd'));
             for ($i = $offset; $i < $offset + $weeks; $i++) {
                 $j = $i - 1;
                 $currMon = date('Ymd', strtotime("monday $j week"));
-                if ($currMon <= $rawtime &&  $rawtime <= date('Ymd', strtotime("saturday $i week"))) {
-                    if (strcmp($lastDate, $rawtime) != 0) {
-                        $lastDate = $rawtime;
-                        $edt->weeks[$i]->days[] = new Day;
-                    }
-                    end($edt->weeks[$i]->days)->events[] = $event;
+                if ($currMon <= $rawtime &&  $rawtime <= date('Ymd', strtotime("sunday $i week"))) {
+                    $day_of_week = $rawtime - $currMon;
+                    $edt->weeks[$i - $offset]->days[$day_of_week]->events[] = $event;
                 }
             }
         }
